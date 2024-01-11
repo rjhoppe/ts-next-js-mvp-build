3c444f9353021174de5ee62d48cf8a3d9207dc43
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2024-01-11 17:45:47 -0500

    feat: Completed initial build of rich text editor component Added rich text editor component to TempEdit page and began refactoring/removing boilerplater.

--- components/TempEdit.tsx
+++ components/TempEdit.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import RecordBreadcrumb from "./RecordBreadcrumb"
 import { Input } from "@nextui-org/input"
+import RichTextEditor from "./RichTextEditor"
+import { Button } from "@nextui-org/button"
+import Link from "next/link"
 
 const TempEdit = () => {
   return (
@@ -9,18 +14,30 @@ const TempEdit = () => {
         type="email"
         label="Recipients"
         defaultValue="example@axon.com"
-        className=" mt-10 max-w-xl"
+        className=" mt-5 max-w-xl"
       />
       <Input 
         type="email"
         label="CC Recipients"
-        className=" mt-10 max-w-xl"
+        className=" mt-5 max-w-xl"
       />
       <Input 
         type="string"
         label="Subject"
-        className=" mt-10 max-w-xl"
+        className=" mt-5 max-w-xl"
       />
+      <p className="mt-5">Message</p>
+      <div className="flex flex-col justify-center mt-5 max-w-xl">
+        <RichTextEditor />
+      </div>
+      <div className="flex mt-20 justify-end gap-5 max-w-xl">
+        <Button href="/" as={Link} className="flex" color="secondary">
+          Cancel
+        </Button>
+        <Button href="/" as={Link} className="flex" color="primary">
+          Save
+        </Button>
+      </div>
     </section>
   )
 }
