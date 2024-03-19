83bff94f43207c376539476c427d828e7abcc0a1
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2024-03-19 10:43:23 -0400

    feat! Added update record capability to rule records Also cleaned up bug where delay default value was not piping into modal upon open

--- queries/put-new-victim.ts
+++ queries/put-new-victim.ts
@@ -1,10 +1,10 @@
-import { TypedSupabaseClient } from '@/utils/types'
-import { InsertVictimTypes } from '@/types/collection'
+// import { TypedSupabaseClient } from '@/utils/types'
+// import { InsertVictimTypes } from '@/types/collection'
 
-export function putNewVictim(client: TypedSupabaseClient, param: string) {
-  return client
-    .from('cases_test_upload')
-    .update({ param })
-    .eq('case_id', `${caseId}`)
-    .select()
-}
+// export function putNewVictim(client: TypedSupabaseClient, param: string) {
+//   return client
+//     .from('cases_test_upload')
+//     .update({ param })
+//     .eq('case_id', `${caseId}`)
+//     .select()
+// }
